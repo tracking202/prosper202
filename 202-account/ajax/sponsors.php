a1981c8ad6e805132d218e2182097a7b6c21c227
Nana <nana@tracking202.com>
2025-04-25 00:25:17 -0700

    Improve sponsor and region handling
    Updated `sponsors.php` to handle invalid or missing JSON data gracefully and improved code formatting. Modified `regions.php` to include a new UI calendar function, adjusted file path logic, and improved code readability.

--- 202-account/ajax/sponsors.php
+++ 202-account/ajax/sponsors.php
@@ -1,28 +1,29 @@
 <?php
-include_once(str_repeat("../", 2).'202-config/connect.php');
+include_once(str_repeat("../", 2) . '202-config/connect.php');
 
 AUTH::require_user();
 
-$json = getUrl( TRACKING202_RSS_URL . '/prosper202/sponsors?type=json');
+$json = getUrl(TRACKING202_RSS_URL . '/prosper202/sponsors?type=json');
 $json = json_decode($json, true);
 
+// Check if $json is not null and contains the 'sponsors' key
+if (is_array($json) && isset($json['sponsors'])) {
+	$sponsors = $json['sponsors'];
+	if (!$sponsors) die();
+	foreach ($sponsors as $sponsor) {
 
+		$html = array_map('htmlentities', $sponsor);
 
-$sponsors = $json['sponsors'];
-if (!$sponsors) die();
-foreach ($sponsors as $sponsor) { 
-	
-	$html = array_map('htmlentities', $sponsor);
- 	
- 		echo '<div class="row app-row" style="margin-bottom: 10px;">';
-  			echo '<div class="col-xs-2">';
-  				echo '<a href="'.$html['url'].'" target="_blank"><img style="width: 42px;" src="'.$html['image'].'"/></a>';
-  			echo '</div>';
-  			echo '<div class="col-xs-10">';
-  				echo '<a href="'.$html['url'].'" target="_blank">'.$html['name'].'</a><br/><span>'.$html['description'].'</span>';
-  			echo '</div>';
-  		echo '</div>';
+		echo '<div class="row app-row" style="margin-bottom: 10px;">';
+		echo '<div class="col-xs-2">';
+		echo '<a href="' . $html['url'] . '" target="_blank"><img style="width: 42px;" src="' . $html['image'] . '"/></a>';
+		echo '</div>';
+		echo '<div class="col-xs-10">';
+		echo '<a href="' . $html['url'] . '" target="_blank">' . $html['name'] . '</a><br/><span>' . $html['description'] . '</span>';
+		echo '</div>';
+		echo '</div>';
+	}
+} else {
+	// Handle the case when JSON is invalid or doesn't contain sponsors
+	echo '<div class="row app-row">No sponsor data available at this time.</div>';
 }
-
-
-
