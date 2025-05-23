54fb9c55d271e66828c0b23516a2e00b73d6cc63
Nana <nana@tracking202.com>
2025-05-23 01:45:39 -0700

    Enhance type safety by explicitly casting user input to string in administration.php

--- 202-account/administration.php
+++ 202-account/administration.php
@@ -6,7 +6,7 @@ include_once(str_repeat("../", 1).'202-config/functions-tracking202.php');
 AUTH::require_user();
 
 $slack = false;
-$mysql['user_own_id'] = $db->real_escape_string($_SESSION['user_own_id']);
+$mysql['user_own_id'] = $db->real_escape_string((string)$_SESSION['user_own_id']);
 $user_sql = "SELECT 2u.user_name as username, 2up.user_slack_incoming_webhook AS url, maxmind_isp, user_time_register, 2up.user_auto_database_optimization_days FROM 202_users AS 2u INNER JOIN 202_users_pref AS 2up ON (2up.user_id = 1) WHERE 2u.user_id = '".$mysql['user_own_id']."'";
 $user_results = $db->query($user_sql);
 $user_row = $user_results->fetch_assoc();
@@ -77,8 +77,8 @@ if (isset($_POST['autocron'])) {
 	    $cron = callAutoCron($endpoint);
 
 	    if ($cron['status'] == 'success') {
-	    	$mysql['auto_cron'] = $db->real_escape_string($_POST['autocron']);
-			$mysql['user_id'] = $db->real_escape_string($_SESSION['user_id']);
+	    	$mysql['auto_cron'] = $db->real_escape_string((string)$_POST['autocron']);
+			$mysql['user_id'] = $db->real_escape_string((string)$_SESSION['user_id']);
 	        $sql = "UPDATE 202_users_pref SET auto_cron = '".$mysql['auto_cron']."' WHERE user_id = '".$mysql['user_id']."'";
 	        $result = _mysqli_query($sql);
 	    }
@@ -91,7 +91,7 @@ if (isset($_POST['maxmind'])) {
 
 	if($_POST['maxmind'] == "true") {
 		if (file_exists(substr(dirname( __FILE__ ), 0,-12) . '/202-config/geo/GeoIPISP.dat')) {
-			$mysql['user_id'] = $db->real_escape_string($_SESSION['user_id']);
+			$mysql['user_id'] = $db->real_escape_string((string)$_SESSION['user_id']);
 			$sql = "UPDATE 202_users_pref SET maxmind_isp='1' WHERE user_id='".$mysql['user_id']."'";
 			$result = _mysqli_query($sql);
 			if ($slack)
@@ -102,7 +102,7 @@ if (isset($_POST['maxmind'])) {
 	} 
 
 	if($_POST['maxmind'] == "false") {
-		$mysql['user_id'] = $db->real_escape_string($_SESSION['user_id']);
+		$mysql['user_id'] = $db->real_escape_string((string)$_SESSION['user_id']);
 		$sql = "UPDATE 202_users_pref SET maxmind_isp='0' WHERE user_id='".$mysql['user_id']."'";
 		$result = _mysqli_query($sql);
 
@@ -144,7 +144,7 @@ if (isset($_POST['database_management'])) {
 }
 
 if (isset($_POST['auto_database_management'])) {
-	$mysq['auto_database_management'] = $db->real_escape_string($_POST['auto_database_management']);
+	$mysq['auto_database_management'] = $db->real_escape_string((string)$_POST['auto_database_management']);
 	$sql ="UPDATE 202_users_pref SET user_auto_database_optimization_days = '".$mysq['auto_database_management']."' WHERE user_id = '".$mysql['user_own_id']."'";
 	$db->query($sql);
 }
@@ -268,7 +268,7 @@ function CronJobLastExecution($datetime) {
 						<p>
 							Default Keyword Preference <span class="pull-right"
 								style="font-size: 10px; line-height: 2.5;">
-				    		<?php  $mysql['user_id'] = $db->real_escape_string($_SESSION['user_id']);
+				    		<?php  $mysql['user_id'] = $db->real_escape_string((string)$_SESSION['user_id']);
 							$user_sql = "SELECT * FROM 202_users_pref WHERE user_id='".$mysql['user_id']."'";
 							$user_result = _mysqli_query($user_sql);
 							$user_row = $user_result->fetch_assoc();
@@ -504,8 +504,8 @@ function CronJobLastExecution($datetime) {
 	        <?php
 			while ($user_log_row = $user_log_result->fetch_assoc()) {
 
-				$html['user_name'] = htmlentities($user_log_row['user_name'], ENT_QUOTES, 'UTF-8');
-				$html['ip_address'] = htmlentities($user_log_row['ip_address'], ENT_QUOTES, 'UTF-8');
+				$html['user_name'] = htmlentities((string)($user_log_row['user_name'] ?? ''), ENT_QUOTES, 'UTF-8');
+				$html['ip_address'] = htmlentities((string)($user_log_row['ip_address'] ?? ''), ENT_QUOTES, 'UTF-8');
 				$html['login_time'] = htmlentities(date('M d, y \a\t g:ia', $user_log_row['login_time']), ENT_QUOTES, 'UTF-8');
 
 				if ($user_log_row['login_success'] == 0) { $html['login_success'] = '<span style="color: #900;">Failed</span>'; } else { $html['login_success'] = 'Passed'; }
